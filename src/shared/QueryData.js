b7d15af894753aae8aca8d7c8c9e691428b81890
AshBardhan <ashbardhan@gmail.com>
2024-05-29 16:57:28 +0530

    Add few more 'QueryData' samples

--- src/shared/QueryData.js
+++ src/shared/QueryData.js
@@ -1,14 +1,26 @@
 export const initialQueryData = [
     {
-        name: "Customers",
-        code: "SELECT * from customers;"
+        name: "All Customers",
+        code: "SELECT * FROM customers;"
+    },
+    {
+        name: "Territory with IDs",
+        code: "SELECT territoryID, territoryName FROM territories;",
     },
     {
         name: "Suppliers From Sales",
-        code: "SELECT * from suppliers WHERE contactTitle='Sales Representative'",
+        code: "SELECT * FROM suppliers WHERE contactTitle='Sales Representative;'",
     },
     {
         name: "Customer with Territory",
-        code: "SELECT customerId, customerName, territoryId, territoryName from customers, territories;"
+        code: "SELECT customerId, customerName, territoryId, territoryName FROM customers, territories;"
+    },
+    {
+        name: "Customers from Dallas",
+        code: "SELECT customerId, customerName, pinCode FROM customers WHERE city='Dallas';"
+    },
+    {
+        name: "All Suppliers",
+        code: "SELECT * FROM suppliers;"
     }
 ];
